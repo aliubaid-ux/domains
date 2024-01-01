83f578a3040eac477c76aa7005545516a054bf51
Ali Ubaid <110494649+aliubaid-ux@users.noreply.github.com>
2024-01-01 18:18:15 +0500

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -15,7 +15,7 @@ if(isset($_POST['btn']))
 
 
 		#Api key
-		$api =array('X-Api-Key:wOqVGIVRYCZ+dd2S77vrvA==QWSBg0GjYzad0UG2');
+		$api =array('X-Api-Key:duOi5aiT4UZZ32B9Fh6NKw==PxNlI8cLcFlJqzLl');
 
 		$ch = curl_init();
 		$timeout=60;
